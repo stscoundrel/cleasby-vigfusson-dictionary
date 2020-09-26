8b5bf2e00f22cc8f52d35dca6140a2dc87cbed54
stscoundrel <sampo@pixels.fi>
2020-09-26 16:49:37 +0300

    Add version without any markup & getter function

--- src/constants/paths.js
+++ src/constants/paths.js
@@ -1,7 +1,9 @@
 const path = require('path')
 
 const CLEASBY_VIGFUSSON_JSON = path.join(`${__dirname}/../../json/cleasby-vigfusson.json`)
+const NO_MARKUP_JSON = path.join(`${__dirname}/../../json/cleasby-vigfusson-no-markup.json`)
 
 module.exports = {
   CLEASBY_VIGFUSSON_JSON,
+  NO_MARKUP_JSON,
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const { read } = require('./services/reader.js')
-const { CLEASBY_VIGFUSSON_JSON } = require('./constants/paths')
+const { CLEASBY_VIGFUSSON_JSON, NO_MARKUP_JSON } = require('./constants/paths')
 
 const getDictionary = () => {
   const words = read(CLEASBY_VIGFUSSON_JSON)
@@ -7,6 +7,13 @@ const getDictionary = () => {
   return words
 }
 
+const getNoMarkupDictionary = () => {
+  const words = read(NO_MARKUP_JSON)
+
+  return words
+}
+
 module.exports = {
   getDictionary,
+  getNoMarkupDictionary,
 }
